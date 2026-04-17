655b06b2e638e8d6f919eebc0e9dd5bd87e83253
jjc73 <thebordelie@yandex.ru>
2026-04-17 16:18:36 +0300

    Add terraform and change ci pipeline

---
+++ cloud-terraform/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file = "./authorized_key.json"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
